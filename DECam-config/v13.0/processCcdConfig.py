d655c5719a8dde75102cd85b0ec5e4a8e2e14cd1
Robert <rbliu@users.noreply.github.com>
2018-03-08 21:41:42 -0500

    Update processCcdConfig.py

--- DECam-config/v13.0/processCcdConfig.py
+++ DECam-config/v13.0/processCcdConfig.py
@@ -2,5 +2,11 @@
 from lsst.obs.decam.decamNullIsr import DecamNullIsrTask
 config.isr.retarget(DecamNullIsrTask)
 
+# Shape measurements with HSM-regauss
+import os.path
+from lsst.utils import getPackageDir
+config.calibrate.measurement.load(os.path.join(getPackageDir("meas_extensions_shapeHSM"), "config", "enable.py"))
+config.calibrate.measurement.plugins["ext_shapeHSM_HsmShapeRegauss"].deblendNChild = "deblend_nChild"
+
 config.calibrate.doAstrometry = False
 config.calibrate.doPhotoCal = False
