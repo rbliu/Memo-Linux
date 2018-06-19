48b892ccc9fc8510019918d767358ba7390bafc2
Robert <rbliu@users.noreply.github.com>
2018-06-19 11:40:49 -0400

    Update setupLSST-ghk1.sh

--- setupLSST-ghk1.sh
+++ setupLSST-ghk1.sh
@@ -9,7 +9,7 @@ which eups
 setup lsst_distrib -t b2954
 echo " "
 echo "############################"
-echo "lsst_distrib has been setup!"
+echo "lsst_distrib (v13.0) has been setup!"
 echo " _     ____ ____ _____           ____  __  __ "
 echo "| |   / ___/ ___|_   _|         |  _ \|  \/  |"
 echo "| |   \___ \___ \ | |    _____  | | | | |\/| |"
