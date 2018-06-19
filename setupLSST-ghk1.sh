4d3340f41eb437cccbeb3d91a9dd2c4baa771eaa
Robert <rbliu@users.noreply.github.com>
2018-06-19 11:30:14 -0400

    Create setupLSST-ghk1.sh

---
+++ setupLSST-ghk1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+LSSTSW=/export/rliu/lsstsw
+. $LSSTSW/bin/setup.sh
+
+echo " "
+echo "You are using EUPS:"
+which eups
+setup lsst_distrib -t b2954
+echo " "
+echo "############################"
+echo "lsst_distrib has been setup!"
+echo " _     ____ ____ _____           ____  __  __ "
+echo "| |   / ___/ ___|_   _|         |  _ \|  \/  |"
+echo "| |   \___ \___ \ | |    _____  | | | | |\/| |"
+echo "| |___ ___) |__) || |   |_____| | |_| | |  | |"
+echo "|_____|____/____/ |_|           |____/|_|  |_|"
+
+echo "############################"
