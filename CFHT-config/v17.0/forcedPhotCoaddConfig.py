1a8711755fe52eccae68e024ed4fc596ae5e2c7e
Robert <rbliu@users.noreply.github.com>
2019-06-28 14:07:27 -0400

    Update forcedPhotCoaddConfig.py

--- CFHT-config/v17.0/forcedPhotCoaddConfig.py
+++ CFHT-config/v17.0/forcedPhotCoaddConfig.py
@@ -7,8 +7,3 @@ config.measurement.slots.modelFlux = "modelfit_CModel"
 config.doApCorr=True
 
 config.measurement.plugins['base_PixelFlags'].masksFpAnywhere.append('CLIPPED')
-
-# extendedness = mag_PSF - mag_CModel
-# ~ 0: star; > 0: galaxy
-config.catalogCalculation.plugins.names = ["base_ClassificationExtendedness"]
-config.measurement.slots.psfFlux = "base_PsfFlux"
