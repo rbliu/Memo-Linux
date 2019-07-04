fcdd1dd8843f822fee234ba462dac039abb48964
rbliu <byliu1990@gmail.com>
2019-07-04 14:14:29 -0400

    Update config

--- CFHT-config/v17.0/forcedPhotCoaddConfig.py
+++ CFHT-config/v17.0/forcedPhotCoaddConfig.py
@@ -1,3 +1,18 @@
+import lsst.meas.extensions.shapeHSM
+hsm_plugins = set([
+    "ext_shapeHSM_HsmShapeBj",      # Bernstein & Jarvis 2002
+    "ext_shapeHSM_HsmShapeLinear",  # Hirata & Seljak 2003
+    "ext_shapeHSM_HsmShapeKsb",     # KSB 1995
+    "ext_shapeHSM_HsmShapeRegauss", # Hirata & Seljak 2003
+    "ext_shapeHSM_HsmSourceMoments",# Not PSF corrected; used by all of the above
+    "ext_shapeHSM_HsmPsfMoments",   # Moments of the PSF, used by all of the above
+])
+config.measurement.plugins.names |= hsm_plugins
+
+config.measurement.slots.shape = "ext_shapeHSM_HsmSourceMoments"
+config.measurement.slots.psfShape = "ext_shapeHSM_HsmPsfMoments"
+config.measurement.plugins["ext_shapeHSM_HsmShapeRegauss"].deblendNChild = "deblend_nChild"
+
 import lsst.meas.modelfit
 import lsst.shapelet
 
@@ -6,4 +21,4 @@ config.measurement.slots.modelFlux = "modelfit_CModel"
 
 config.doApCorr=True
 
-config.measurement.plugins['base_PixelFlags'].masksFpAnywhere.append('CLIPPED')
+#config.measurement.plugins['base_PixelFlags'].masksFpAnywhere.append('CLIPPED')
